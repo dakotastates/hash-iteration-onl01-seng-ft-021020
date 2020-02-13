8719bf1efbda9608e5ffbb8d803cd552db3270d7
Dakota States <dakota.states@gmail.com>
2020-02-13 23:28:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,5 +14,10 @@ end
  
 
 age_appropriate_birthday(birthday_kids)
-
+def happy_birthday(birthday_kids)
+ birthday_kids.each do |kids_name, age|
+  
+   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+ end
+end
 end
